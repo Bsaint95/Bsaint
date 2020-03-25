e57910cba919799cc3ef15d038efff62dfd6c873
Bsaint <62014264+Bsaint95@users.noreply.github.com>
2020-03-25 04:35:38 +0100

    Add files via upload

---
+++ Python_Task.py
@@ -0,0 +1,13 @@
+#import math module to get the value of pi
+import math
+
+# r is radius of a. circle
+#A is Area of a circle
+
+#read radius of the circle 
+r= float(input("Enter radius value"))
+
+#calculate area
+A= math.pi * (r ** 2)
+
+print(A)
